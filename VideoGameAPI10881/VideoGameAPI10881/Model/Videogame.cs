907a737c188cef0c081a34ad03e4929423aebf07
AzizNorboev <norboev6170@gmail.com>
2021-11-01 16:11:46 +0500

    Created Models

---
+++ VideoGameAPI10881/VideoGameAPI10881/Model/Videogame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoGameAPI10881.Model
+{
+    public class Videogame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+        public string Publisher { get; set; }
+
+    }
+}
